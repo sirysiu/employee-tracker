6c23f7e02a606f92d6950b5eb9a94caa194288df
siry siu <144174351+sirysiu@users.noreply.github.com>
2024-01-24 14:04:21 -0500

    added add employee

--- index.js
+++ index.js
@@ -85,6 +85,111 @@ if (answers.view === 'View All Roles') {
         
     })
 };
+if (answers.view === 'Add Employees') {
+   
+    inquirer
+        .prompt([
+            {
+                name: "employee_firstName",
+                type: "input",
+                message: "Employee first name?",
+            },
+            {
+                name: "employee_lastName",
+                type: "input",
+                message: "Employee last name?",
+            },
+            {
+                name: "employee_role",
+                type: "input",
+                message: "What role are they?",
+            },
+            {
+                name: "employee_manager",
+                type: "input",
+                message: "Manager last name?",
+            },
+        ])
+        .then((response) => {
+
+            const insertEmployeeSql = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+
+            selectRoleIdSql = "SELECT id FROM role WHERE title = ?";
+            selectManagerIdSql = "SELECT id FROM employee WHERE last_name = ?";
+
+            // This gets role Id based on the role name. 
+            db.query(selectRoleIdSql, [response.employee_role], (error, roleResults) => {
+                if (error) {
+                    console.log("Error retieving role ID:", error);
+                    
+                } else {
+                    // This checks to make sure we retreived the Id from the role
+                    const roleId = roleResults[0] ? roleResults[0].id : null;
+
+                    if (roleId !== null) {
+
+                        if (response.employee_manager.trim() !== '') {
+                            // This will retreive the manager_id based on manager last name.
+                            db.query(selectManagerIdSql, [response.employee_manager], (error, managerResults) => {
+                                if (error) {
+                                    console.log("Error retrieving manager ID: ", error);
+                                    
+                                } else {
+                                    const managerId = managerResults[0] ? managerResults[0].id : null;
+
+                                    if (managerId !== null) {
+                                        db.query(
+                                            insertEmployeeSql, [response.employee_firstName, response.employee_lastName, roleId, managerId], (error, results) => {
+                                                if (error) {
+                                                    console.log("Error inserting into db: ", error);
+                                                } else {
+                                                    console.log("Added employee: ", response.employee_firstName, response.employee_lastName);
+                                                }
+
+                                                
+                                            }
+                                        );
+                                    } else {
+                                        console.log("Manager not found. Employee not added.");
+                                        
+                                    }
+                                }
+                            });
+                        } else {
+                            db.query(insertEmployeeSql, [response.employee_firstName, response.employee_lastName, roleId, null], (error, results) => {
+                                if (error) {
+                                    console.log("Error inserting into db: ", error);
+                                } else {
+                                    console.log("Added employee:", response.employee_firstName, response.employee_lastName);
+                                }
+
+                               
+                            })
+                        }
+
+                    } else {
+                        console.log("Role not found. Employee not added.");
+                       
+                    }
+                }
+            });
+
+        });
+};
+
+if (answers.view === 'View All Departments') {
+   // This SQL query selects all departments and displays them in a table. 
+   db.query('SELECT id AS Department_ID, name AS Department_Name FROM department;', (error, results) => {
+    if (error) {
+        console.log("Error getting query: ", error);
+    } else {
+        console.log("View All Departments:");
+        console.table(results)
+    }
+
+   
+})
+};
 if (answers.view === 'Exit') {
     process.exit();
 }
